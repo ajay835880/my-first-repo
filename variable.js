af211cb45e31796d40dc88489e66d307fff2bea5
ajay835880 <ajaykushwaha68968@gmail.com>
2024-05-14 00:01:33 +0530

    first commit

---
+++ variable.js
@@ -0,0 +1,49 @@
+let a = 40;
+let b = 50;
+let c = 60;
+let d = 90;
+let e = a+b+c+d;
+
+
+console.log()
+
+let f = 79+true;
+
+console.log()
+
+let g = 81-true;
+console.log(e)
+let s = 9999;
+let r = 999;
+let v = 99;
+let n = 9;
+let w =true;
+
+
+console.log()
+
+let j = 6;
+let m = 50;
+let l = 60;
+
+console.log()
+
+let k = 60;
+let h = 2;
+
+console.log()
+
+let q = 9;
+
+console.log("Addition: q+3=",q+3);
+console.log("subtraction: q-3=",q-3);
+console.log("multiplication: q*3=",q*3);
+console.log("division: q/3=",q/3);
+console.log("Remainder: q%3=",q&3);
+console.log("Increment:++q=",++q);
+console.log("Decrement:--q=",--q);
+console.log("Exponentiation:q**3=",q**3);
+
+
+const t=3,z=2;
+console.log(a>b);
