299c4e644979518dc343b7e59712fd49da242284
ajay835880 <ajaykushwaha68968@gmail.com>
2024-05-21 12:41:16 +0530

    first commit

---
+++ operators.js
@@ -0,0 +1,76 @@
+///////////////////////////////////////////operator///////////////////////////////////////////////////////
+// 1 Arithimatic operators
+// 2 Assignment operators
+// 3 comperission operators
+// 4 logical operators
+// 5 string operators
+// 6 bitwise operators
+// 7 sprcial operators
+
+
+////////////////////////////////////arithimatic operators//////////////
+
+// var a = 10;
+// var b = 20;
+// var result = 10+20;
+// console.log(result)
+
+// var a = 10;
+// var b = 20;
+// var result = 20-10;
+// console.log(result)
+
+// var a = 10;
+// var b = 20;
+//  var result = 10*20;
+//  console.log(result)
+
+//  var a = 20;
+//  var b = 10;
+//  var result = 20 / 10;
+//  console.log(result)
+
+////////////////////assignment operator////////////////////
+
+// var a = 10;
+// var b = 20;
+// a += b //  a = a+b
+
+// console.log(a);
+ 
+// var a = 20;
+// var b = 10;
+//  a -= b  ///a = a-b
+
+//  console.log(a)
+
+// var a = 10;
+// var b = 20;
+//  a *= b
+//  console.log(a)
+
+// var a = 20;
+// var b = 10;
+// a /= b 
+// console.log(a)
+
+///////////////////// comperision operator//////////
+
+// var a = 5;
+// var b = 5;
+// console.log(a===b);
+
+// var a = 5;
+// var b = "5"
+// console.log(a===b);
+
+// var a = 5;
+// var b = "5";
+
+// console.log(a===b);
+
+// var a = 10;
+// var b = 10;
+// console.log(a!==b);
+
+////////////////////////////logical operator///////////////
